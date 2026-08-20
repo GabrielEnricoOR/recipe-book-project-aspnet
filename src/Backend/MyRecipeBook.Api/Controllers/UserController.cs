5383ce214e07b7908424c20a9a1e85418aebce86
Gabriel Ramos <gabrielenrico0123@gmail.com>
2026-08-20 14:55:31 -0300

    feat: add method to register a user

--- src/Backend/MyRecipeBook.Api/Controllers/UserController.cs
+++ src/Backend/MyRecipeBook.Api/Controllers/UserController.cs
@@ -2,9 +2,12 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace MyRecipeBook.Api.Controllers;
 
+[Route("api/[controller]")]
 [ApiController]
-[Route("[controller]")]
 public class UserController : ControllerBase
 {
-    
+    public IActionResult Register()
+    {
+        return Created();
+    }
 }
