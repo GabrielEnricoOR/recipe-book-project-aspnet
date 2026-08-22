b6ebe9422eab822ad57c8af59b6f809c4ef74bc9
Gabriel Ramos <gabrielenrico0123@gmail.com>
2026-08-22 11:13:52 -0300

    add a simple validation to user name, password and email

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Data;
+using FluentValidation;
+using MyRecipeBook.Communication;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUserAccountJson>
+{
+    public RegisterUserAccountValidator()
+    {
+        RuleFor(user => user.Name).NotEmpty().WithMessage("Name is required");
+        RuleFor(user => user.Email).NotEmpty().WithMessage("Email is required");
+        RuleFor(user => user.Password).NotEmpty().WithMessage("Password is required");
+        When(user => string.IsNullOrEmpty(user.Email) == false, () =>
+        {
+           RuleFor(user => user.Email).EmailAddress().WithMessage("Email is not valid");
+        });
+    }
+}
