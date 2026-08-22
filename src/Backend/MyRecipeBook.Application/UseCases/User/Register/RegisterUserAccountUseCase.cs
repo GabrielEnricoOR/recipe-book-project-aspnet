bc2e0d2ac5b2e77e38b9fad032fcb1a4abba7bc6
Gabriel Ramos <gabrielenrico0123@gmail.com>
2026-08-21 23:04:57 -0300

    feat: add a User case to register a user

---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -0,0 +1,12 @@
+using System;
+using MyRecipeBook.Communication;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountUseCase
+{
+    public void Execute(RequestRegisterUserAccountJson request)
+    {
+        
+    }
+}
