ccfabfe5b50ba3aa4ba397d7f2cf192f90d2b52b
Gabriel Ramos <gabrielenrico0123@gmail.com>
2026-09-02 20:58:27 -0300

    feat: add first application project service in DI

---
+++ src/Backend/MyRecipeBook.Application/DependencyInjectionExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Application.UseCases.User.Register;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipeBook.Application
+{
+    public static class DependencyInjectionExtension
+    {
+        public static void AddApplicationServices(IServiceCollection services)
+        {
+            services.AddScoped<IRegisterUserAccountUseCase, RegisterUserAccountUseCase>();
+        }
+    }
+}
