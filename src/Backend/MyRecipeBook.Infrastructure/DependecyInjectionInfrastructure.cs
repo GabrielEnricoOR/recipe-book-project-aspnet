98cb0bd533a9ca69e040907e12d1ab8681211a44
Gabriel Ramos <gabrielenrico0123@gmail.com>
2026-09-02 20:55:55 -0300

    feat: add first infra service in DI

---
+++ src/Backend/MyRecipeBook.Infrastructure/DependecyInjectionInfrastructure.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Security.PasswordHashing;
+using MyRecipeBook.Infrastructure.Security.PasswordHashing;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyRecipeBook.Infrastructure
+{
+    public static class DependecyInjectionInfrastructure
+    {
+        public static void AddInfrastructureServices(IServiceCollection services)
+        {
+            services.AddScoped<IPasswordHashing, Argon2PasswordHasher>();
+        }
+
+    }
+}
