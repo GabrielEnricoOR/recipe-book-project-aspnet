02a5a5c589b885d74deb4bc04e036b3de838ad25
GabrielEnricoOR <gabrielenrico0123@gmail.com>
2026-09-25 11:45:55 -0300

    test: add invalid register user account validation tests

--- Validation.Tests/User/Register/RegisterUserAccountValidatorTests.cs
+++ Validation.Tests/User/Register/RegisterUserAccountValidatorTests.cs
@@ -1,6 +1,7 @@
 using CommomTestUtilities;
 using MyRecipeBook.Application.UseCases.User.Register;
 using MyRecipeBook.Communication;
+using MyRecipeBook.Exception;
 using Shouldly;
 
 namespace Validation.Tests.User.Register;
@@ -19,5 +20,81 @@ public class RegisterUserAccountValidatorTests
         var result = validator.Validate(request);
         
         result.IsValid.ShouldBeTrue();
+    }
+
+    [Fact]
+    public void Validate_ShouldHaveError_WhenNameIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Builder();
+        request.Name = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+        
+        result.IsValid.ShouldBeFalse();
+        
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_NAME_REQUIRED));
+        });
+    }
+    
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Builder();
+        request.Email = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+        
+        result.IsValid.ShouldBeFalse();
+        
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_REQUIRED));
+        });
+    }
+    
+    [Fact]
+    public void Validate_ShouldHaveError_WhenPasswordIsEmpty()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Builder();
+        request.Password = string.Empty;
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+        
+        result.IsValid.ShouldBeFalse();
+        
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_PASSWORD_REQUIRED));
+        });
+    }
+    
+    [Fact]
+    public void Validate_ShouldHaveError_WhenEmailIsNotValid()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Builder();
+        request.Email = "recipe123";
+
+        var validator = new RegisterUserAccountValidator();
+
+        var result = validator.Validate(request);
+        
+        result.IsValid.ShouldBeFalse();
+        
+        result.Errors.ShouldSatisfyAllConditions(error =>
+        {
+            error.Count.ShouldBe(1);
+            error.ShouldContain(error => error.ErrorMessage.Equals(ResourceMessagesException.VALIDATION_EMAIL_INVALID));
+        });
     }
 }
